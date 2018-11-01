4ebfc30056f563a56675d0ab867d0a04973c0800
USER <user@usernomacbook-air.local>
2018-11-01 13:55:35 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by USER on 2018/11/01.
+//  Copyright © 2018年 仙台東部不動産. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
